43fe791af9056dbda4f8de528240e741faf22cb4
Andres Parra <120739623+byandrev@users.noreply.github.com>
2025-10-20 07:46:53 -0500

    feat: add expression validation with stack implementationg
    Implemented expression validation using a stack to check for matching parentheses, braces, and brackets.

--- retos/reto1_stack.py
+++ retos/reto1_stack.py
@@ -16,8 +16,22 @@ Tips:
 from estructuras.stack import Stack
 
 PARES = {')': '(', '}': '{', ']': '['}
+OPENINGS = ["(", "{", "["]
+ENDINGS = [")", "}", "]"]
 
 def validate_expression(expression: str) -> bool:
-    # TODO: Implementar con Stack siguiendo las reglas de arriba.
-    # Debe ser O(n) en tiempo; O(n) espacio peor caso.
-    raise NotImplementedError
+	# TODO: Implementar con Stack siguiendo las reglas de arriba.
+	# Debe ser O(n) en tiempo; O(n) espacio peor caso.
+	stack_structures = Stack()
+	
+	for item in expression:
+		if item in OPENINGS:
+			stack_structures.push(item)
+		elif item in ENDINGS:
+			if stack_structures.size() == 0:
+				return False
+			else:
+				if stack_structures.peek() == PARES[item]:
+					popped = stack_structures.pop()
+	
+	return stack_structures.is_empty()
