f6f4963b7cb8bd25b5e7106d7d944946cdb50535
Andres Parra <byandrev@gmail.com>
2025-10-22 17:37:50 -0500

    style: remove unnecessary whitespace in test cases for validate_expression

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -9,32 +9,32 @@ class TestChallenge1Stack(unittest.TestCase):
         resultado = validate_expression(expresion)
         # Assert
         self.assertTrue(resultado)
-    
+
     def extra_closure_imbalance(self):
         expresion = "({[]}))"
         resultado = validate_expression(expresion)
         self.assertFalse(resultado)
-        
+
     def incorrect_order(self):
         expresion = "{[}]"
         resultado = validate_expression(expresion)
         self.assertFalse(resultado)
-        
+
     def empty_string(self):
         expresion = ""
         resultado = validate_expression(expresion)
         self.assertTrue(resultado)
-        
+
     def only_openings(self):
         expresion = ""
         resultado = validate_expression(expresion)
         self.assertTrue(resultado)
-        
+
     def single_type(self):
         expresion = "{}"
         resultado = validate_expression(expresion)
         self.assertTrue(resultado)
-        
+
 
 if __name__ == "__main__":
     unittest.main()
